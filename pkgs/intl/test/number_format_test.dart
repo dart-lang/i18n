dart-archive/intl

commit 6524e278eb214a74d7179a808693019a7322be5e

Group padding digits to the left of the number
-------------
Created by MOE: https://github.com/google/moe
MOE_MIGRATED_REVID=160470967

lib/src/intl/number_format.dart
@@ -761,7 +761,10 @@ class NumberFormat {
         fractionDigits > 0 && (minimumFractionDigits > 0 || fractionPart > 0);
 
     if (_hasIntegerDigits(integerDigits)) {
-      _padEmpty(minimumIntegerDigits - digitLength);
+      // Add the padding digits to the regular digits so that we get grouping.
+      var padding = symbols.ZERO_DIGIT * (minimumIntegerDigits - digitLength);
+      integerDigits = "$padding$integerDigits";
+      digitLength = integerDigits.length;
       for (var i = 0; i < digitLength; i++) {
         _addDigit(integerDigits.codeUnitAt(i));
         _group(digitLength, i);

test/number_format_test.dart
@@ -105,6 +105,24 @@ main() {
     }
   });
 
+  test('Padding left', () {
+    var expected = [
+      '1',
+      '1',
+      '01',
+      '001',
+      '0,001',
+      '00,001',
+      '000,001',
+      '0,000,001'
+    ];
+    for (var i = 0; i < 7; i++) {
+      var f = new NumberFormat.decimalPattern();
+      f.minimumIntegerDigits = i;
+      expect(f.format(1), expected[i]);
+    }
+  });
+
   test('Exponential form', () {
     var number = new NumberFormat("#.###E0");
     for (var x in testExponential.keys) {

pubspec.yaml
@@ -1,5 +1,5 @@
 name: intl
-version: 0.15.1
+version: 0.15.2-dev
 author: Dart Team <misc@dartlang.org>
 description: Contains code to deal with internationalized/localized messages, date and number formatting and parsing, bi-directional text, and other internationalization issues.
 homepage: https://github.com/dart-lang/intl

dart-lang/i18n

commit 85750aa701081674a9cb1d0695507b39c45bfd59

Fix a regression introduced with grouping of padding digits. We started compensating twice for locales with non-ascii digits. Regression was introduced in 6524e278eb214a74d7179a808693019a7322be5e MOE_MIGRATED_REVID=160470967
-------------
Created by MOE: https://github.com/google/moe
MOE_MIGRATED_REVID=171899248

pkgs/intl/lib/src/intl/number_format.dart
@@ -762,7 +762,7 @@ class NumberFormat {
 
     if (_hasIntegerDigits(integerDigits)) {
       // Add the padding digits to the regular digits so that we get grouping.
-      var padding = symbols.ZERO_DIGIT * (minimumIntegerDigits - digitLength);
+      var padding = '0' * (minimumIntegerDigits - digitLength);
       integerDigits = "$padding$integerDigits";
       digitLength = integerDigits.length;
       for (var i = 0; i < digitLength; i++) {

pkgs/intl/test/number_format_test.dart
@@ -244,6 +244,12 @@ main() {
   });
 
   testSimpleCurrencySymbols();
+
+  test('Padding digits with non-ascii zero', () {
+    var format = new NumberFormat('000', 'ar');
+    var padded = format.format(0);
+    expect(padded, '٠٠٠');
+  });
 }
 
 String stripExtras(String input) {