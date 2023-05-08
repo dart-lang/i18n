7aa91ccd62968345c5bac18aa46010f0fc4186d1
Moritz <mosum@google.com>
2023-05-08 16:48:50 +0200

    Add docs to localematcher

--- pkgs/intl4x/lib/src/options.dart
+++ pkgs/intl4x/lib/src/options.dart
@@ -1,7 +1,24 @@
+/// How to pick the best match between a user's desired locales and an
+/// application's supported locales.
+///
+/// When receiving a list of locales for which there is no perfect match in the
+/// list of supported locales, it is probably not the best solution to return
+/// null or an empty string.
+/// Instead, the application "falls back" until it finds a matching language tag
+/// associated with a suitable piece of content to insert. The exact fallback
+/// algorithm is determined by this enum.
 enum LocaleMatcher {
-  lookup('lookup'),
+  /// See the algorithm in
+  /// https://datatracker.ietf.org/doc/html/rfc4647#section-3.4.
+  lookup,
+
+  /// A matcher lets the runtime provide a locale that's at least, but possibly
+  /// more, suited for the request than the result of the [lookup] algorithm.
   bestfit('best fit');
 
-  final String jsName;
-  const LocaleMatcher(this.jsName);
+  final String? _jsName;
+
+  String? get jsName => _jsName ?? name;
+
+  const LocaleMatcher([this._jsName]);
 }
