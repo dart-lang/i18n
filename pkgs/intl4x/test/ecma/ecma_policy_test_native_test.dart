c5e3da92bec30a8c47a5936a4920dc94101c5aa9
Moritz <mosum@google.com>
2023-05-08 11:38:08 +0200

    Fix names

--- pkgs/intl4x/test/ecma/ecma_policy_test_native_test.dart
+++ pkgs/intl4x/test/ecma/ecma_policy_test_native_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-@TestOn('native')
+@TestOn('vm')
 library;
 
 import 'package:intl4x/intl4x.dart';
