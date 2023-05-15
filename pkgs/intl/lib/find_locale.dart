1fa37448b5868322781b004f8aef6ac92f596a9a
Moritz <mosum@google.com>
2023-05-15 19:50:49 +0200

    Add stub for conditional import (#650)

--- pkgs/intl/lib/find_locale.dart
+++ pkgs/intl/lib/find_locale.dart
@@ -1,4 +1,5 @@
 export 'src/intl_default.dart' // Stub implementation
     // Browser implementation
     if (dart.library.html) 'intl_browser.dart'
+    // Native implementation
     if (dart.library.io) 'intl_standalone.dart';
