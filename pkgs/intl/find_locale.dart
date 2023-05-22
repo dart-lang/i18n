eff97ec17d6e17812856b67215858983e3782f85
Googler <copybara-servicebot@google.com>
2023-05-22 10:26:19 -0700

    Internal change
    PiperOrigin-RevId: 534108282

--- pkgs/intl/find_locale.dart
+++
@@ -1,4 +0,0 @@
-export 'lib/intl_default.dart' // Stub implementation
-    // Browser implementation
-    if (dart.library.html) 'intl_browser.dart'
-    if (dart.library.io) 'intl_standalone.dart';
