beca88a0ed0ac8c0358029c1ca134a319982b687
Moritz <mosum@google.com>
2023-08-22 16:14:06 +0200

    Add full ECMA locale (#704)
    * Add full ECMA locale
    
    * Remove toString
    
    * Dart fix
    
    * Change pubspec
    
    * Skip web only tests
    
    * Temp fix

--- pkgs/intl4x/lib/src/display_names/display_names_stub.dart
+++ pkgs/intl4x/lib/src/display_names/display_names_stub.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import '../locale.dart';
+import '../locale/locale.dart';
 import '../options.dart';
 import 'display_names_impl.dart';
 
