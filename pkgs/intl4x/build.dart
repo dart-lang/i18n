6b7acf3f9307805ee7978f44c496daab0c6bd3f7
Moritz <mosum@google.com>
2023-08-29 11:40:31 +0200

    Add compilation step

--- pkgs/intl4x/build.dart
+++ pkgs/intl4x/build.dart
@@ -9,22 +9,59 @@ import 'package:native_assets_cli/native_assets_cli.dart';
 void main(List<String> args) async {
   final buildConfig = await BuildConfig.fromArgs(args);
   final fileName = 'icu4x${Target.current.toString()}';
+  final shouldCompile = buildConfig.config.bool('compile');
 
-  final file = await downloadAsset(fileName, fileName);
-
-  final asset = Asset(
+  File library;
+  if (shouldCompile) {
+    library = await compileAsset(buildConfig.target, fileName);
+  } else {
+    library = await downloadAsset(fileName, fileName);
+  }
+  final libraryAsset = Asset(
     id: fileName,
-    path: AssetRelativePath(file.uri),
+    path: AssetRelativePath(library.uri),
     linkMode: LinkMode.dynamic,
     target: Target.current,
   );
 
   final buildOutput = BuildOutput(
     timestamp: DateTime.timestamp(),
-    assets: [asset],
+    assets: [libraryAsset],
   );
 
   await buildOutput.writeToFile(outDir: buildConfig.outDir);
+}
+
+Future<File> compileAsset(Target target, String fileName) async {
+  final file = File(fileName);
+  await file.create();
+  const icuDatagen = IcuDatagen(
+    keys: [
+      'datetime/japanese/datesymbols@1',
+      'datetime/gregory/datelengths@1',
+      'datetime/timelengths@1',
+      'decimal/symbols@1'
+    ],
+    locales: ['ja'],
+  );
+  final dataGenerationProcess = await icuDatagen.runDataGeneration();
+
+  throwOnError(dataGenerationProcess, 'icu4x-datagen', [icuDatagen.toString()]);
+
+  final processResult = await Process.run('cargo', [
+    'build',
+    '--release',
+  ]);
+  throwOnError(processResult, 'cargo', ['build', '--release']);
+
+  return file;
+}
+
+void throwOnError(ProcessResult dataGenerationProcess, String executable,
+    List<String> arguments) {
+  if (dataGenerationProcess.exitCode != 0) {
+    throw ProcessException(executable, arguments, 'Error running $executable');
+  }
 }
 
 Future<File> downloadAsset(String fileName, String assetName) async {
@@ -38,4 +75,45 @@ Future<File> downloadAsset(String fileName, String assetName) async {
   }
   await response.pipe(file.openWrite());
   return file;
+}
+
+class IcuDatagen {
+  final List<String> keys;
+  final List<String> locales;
+  final Format format;
+
+  const IcuDatagen({
+    required this.keys,
+    required this.locales,
+    this.format = Format.mod,
+  });
+
+  Future<ProcessResult> runDataGeneration() async {
+    final processResult =
+        await Process.run('cargo', ['install', 'icu_datagen']);
+
+    throwOnError(processResult, 'cargo', ['install', 'icu_datagen']);
+
+    return Process.run('icu4x-datagen', [
+      '--keys',
+      ...keys,
+      '--locales',
+      ...locales,
+      '--format',
+      format.name,
+      '--out',
+      'my_data_blob.postcard',
+      '--overwrite',
+    ]);
+  }
+
+  @override
+  String toString() =>
+      'IcuDatagen(keys: $keys, locales: $locales, format: $format)';
+}
+
+enum Format {
+  dir,
+  blob,
+  mod,
 }
