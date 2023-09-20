d57f0818e9ed3ba4fb48b2f1abbc30ef934461d0
Moritz <mosum@google.com>
2023-09-20 13:52:59 +0200

    Remove unneeded

--- pkgs/messages_shrinker/lib/messages_shrinker.dart
+++ pkgs/messages_shrinker/lib/messages_shrinker.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library message_shrinker;
 
-export 'src/messages_shrinker_base.dart';
+export 'src/messages_shrinker.dart';
 
 // TODO: Export any libraries intended for clients of this package.
