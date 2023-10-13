6a95e4a05734c3994a7c07de7ec8dff762b06ce9
Moritz <mosum@google.com>
2023-10-13 07:51:06 -0600

    Remove empty scripts

--- pkgs/messages/link.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:messages_shrinker/messages_shrinker.dart';
-
-void main(List<String> args) {
-  // String dataFile;
-  // String constInstancesFile;
-  // String outputFile;
-  // MessageShrinker().shrink(dataFile, constInstancesFile, outputFile);
-}
