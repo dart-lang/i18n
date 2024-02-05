12c93630d48771a6c4a66ea6fe0616b72a4316e4
Moritz <mosum@google.com>
2024-02-05 16:12:04 +0100

    Working on matching options

--- pkgs/intl4x/lib/src/number_format/number_format_4x.dart
+++ pkgs/intl4x/lib/src/number_format/number_format_4x.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:math';
+
 import '../bindings/lib.g.dart' as icu;
 import '../data.dart';
 import '../data_4x.dart';
@@ -20,12 +22,15 @@ class NumberFormat4X extends NumberFormatImpl {
       : _formatter = icu.FixedDecimalFormatter.withGroupingStrategy(
           data.to4X(),
           locale.to4X(),
-          options.to4xOptions(),
+          options.groupingStrategy4X(),
         );
 
   @override
-  String formatImpl(Object number) =>
-      _formatter.format(_toFixedDecimal(number));
+  String formatImpl(Object number) {
+    final fixedDecimal = _toFixedDecimal(number);
+    final format = _formatter.format(fixedDecimal);
+    return format;
+  }
 
   icu.FixedDecimal _toFixedDecimal(Object number) {
     final icu.FixedDecimal fixedDecimal;
@@ -35,15 +40,85 @@ class NumberFormat4X extends NumberFormatImpl {
       final String s => icu.FixedDecimal.fromString(s),
       Object() => icu.FixedDecimal.fromString(number.toString()),
     };
+    return _constructDouble(fixedDecimal);
+  }
+
+  icu.FixedDecimal _constructDouble(icu.FixedDecimal fixedDecimal) {
+    fixedDecimal.padStart(options.minimumIntegerDigits);
+    final minFractionDigits = options.digits?.fractionDigits.$1;
+    final maxFractionDigits = options.digits?.fractionDigits.$2;
+    final minSignificantDigits = options.digits?.significantDigits.$1;
+    final maxSignificantDigits = options.digits?.significantDigits.$2;
+
+    final overhead = fixedDecimal.length - (maxSignificantDigits ?? 0);
+    final maxSignificantPosition = fixedDecimal.magnitudeStart + overhead;
+    final maxFractionPosition =
+        max(fixedDecimal.magnitudeStart, -(maxFractionDigits ?? 0));
+
+    final roundingPriority = options.digits?.roundingPriority;
+    final bool? useSignificant;
+    if (maxFractionDigits != null &&
+        maxSignificantDigits != null &&
+        roundingPriority != null) {
+      useSignificant = switch (roundingPriority) {
+        RoundingPriority.auto => true,
+        RoundingPriority.morePrecision =>
+          maxSignificantPosition < maxFractionPosition,
+        RoundingPriority.lessPrecision =>
+          maxSignificantPosition > maxFractionPosition,
+      };
+    } else {
+      useSignificant = null;
+    }
+
+    if (minFractionDigits != null) {
+      fixedDecimal.padEnd(-minFractionDigits);
+    }
+    if (maxFractionDigits != null && !(useSignificant ?? false)) {
+      _roundDecimal(fixedDecimal, maxFractionPosition);
+    }
+    if (minSignificantDigits != null &&
+        fixedDecimal.length < minSignificantDigits) {
+      final missingZeroes = minSignificantDigits - fixedDecimal.length;
+      fixedDecimal.padEnd(fixedDecimal.magnitudeStart - missingZeroes);
+    }
+    if (maxSignificantDigits != null &&
+        fixedDecimal.length > maxSignificantDigits &&
+        (useSignificant ?? true)) {
+      _roundDecimal(fixedDecimal, maxSignificantPosition);
+    }
     return fixedDecimal;
+  }
+
+  void _roundDecimal(
+      icu.FixedDecimal fixedDecimal, int maxSignificantPosition) {
+    final roundingFunction = switch (options.roundingMode) {
+      RoundingMode.ceil => fixedDecimal.ceil,
+      RoundingMode.floor => fixedDecimal.floor,
+      RoundingMode.expand => fixedDecimal.expand,
+      RoundingMode.trunc => fixedDecimal.trunc,
+      RoundingMode.halfCeil => fixedDecimal.halfCeil,
+      RoundingMode.halfFloor => fixedDecimal.halfFloor,
+      RoundingMode.halfExpand => fixedDecimal.halfExpand,
+      RoundingMode.halfTrunc => fixedDecimal.halfTrunc,
+      RoundingMode.halfEven => fixedDecimal.halfEven,
+    };
+    roundingFunction(maxSignificantPosition);
   }
 }
 
 extension on NumberFormatOptions {
-  icu.FixedDecimalGroupingStrategy to4xOptions() => switch (useGrouping) {
+  icu.FixedDecimalGroupingStrategy groupingStrategy4X() =>
+      switch (useGrouping) {
         Grouping.always => icu.FixedDecimalGroupingStrategy.always,
         Grouping.auto => icu.FixedDecimalGroupingStrategy.auto,
         Grouping.never => icu.FixedDecimalGroupingStrategy.never,
         Grouping.min2 => icu.FixedDecimalGroupingStrategy.min2,
       };
+}
+
+extension on icu.FixedDecimal {
+  int get length => fractionLength + integerLength;
+  int get fractionLength => max(0, -magnitudeStart);
+  int get integerLength => max(0, magnitudeEnd + 1);
 }
--- pkgs/intl4x/test/numberformat_test.dart
+++ pkgs/intl4x/test/numberformat_test.dart
@@ -9,37 +9,127 @@ import 'package:test/test.dart';
 import 'utils.dart';
 
 void main() {
-  group('Some manual tests', () {
-    final intl = Intl(locale: const Locale(language: 'en', region: 'US'));
+  final intl = Intl(locale: const Locale(language: 'en', region: 'US'));
+  group('grouping', () {
+    testWithFormatting('always', () {
+      final numberFormatOptions = intl.numberFormat(
+          NumberFormatOptions.custom(useGrouping: Grouping.always));
+      expect(numberFormatOptions.format(1000), '1,000');
+      expect(numberFormatOptions.format(10000), '10,000');
+    });
 
-    group('grouping', () {
-      testWithFormatting('always', () {
-        final numberFormatOptions = intl.numberFormat(
-            NumberFormatOptions.custom(useGrouping: Grouping.always));
-        expect(numberFormatOptions.format(1000), '1,000');
-        expect(numberFormatOptions.format(10000), '10,000');
-      });
+    testWithFormatting('never', () {
+      final numberFormatOptions = intl.numberFormat(
+          NumberFormatOptions.custom(useGrouping: Grouping.never));
+      expect(numberFormatOptions.format(1000), '1000');
+      expect(numberFormatOptions.format(10000), '10000');
+    });
 
-      testWithFormatting('never', () {
-        final numberFormatOptions = intl.numberFormat(
-            NumberFormatOptions.custom(useGrouping: Grouping.never));
-        expect(numberFormatOptions.format(1000), '1000');
-        expect(numberFormatOptions.format(10000), '10000');
-      });
+    testWithFormatting('auto', () {
+      final numberFormatOptions = intl
+          .numberFormat(NumberFormatOptions.custom(useGrouping: Grouping.auto));
+      expect(numberFormatOptions.format(1000), '1,000');
+      expect(numberFormatOptions.format(10000), '10,000');
+    });
 
-      testWithFormatting('auto', () {
-        final numberFormatOptions = intl.numberFormat(
-            NumberFormatOptions.custom(useGrouping: Grouping.auto));
-        expect(numberFormatOptions.format(1000), '1,000');
-        expect(numberFormatOptions.format(10000), '10,000');
-      });
+    testWithFormatting('min2', () {
+      final numberFormatOptions = intl
+          .numberFormat(NumberFormatOptions.custom(useGrouping: Grouping.min2));
+      expect(numberFormatOptions.format(1000), '1000');
+      expect(numberFormatOptions.format(10000), '10,000');
+    });
+  });
 
-      testWithFormatting('min2', () {
-        final numberFormatOptions = intl.numberFormat(
-            NumberFormatOptions.custom(useGrouping: Grouping.min2));
-        expect(numberFormatOptions.format(1000), '1000');
-        expect(numberFormatOptions.format(10000), '10,000');
-      });
+  testWithFormatting('fractionDigits', () {
+    String formatter(Object number) => intl
+        .numberFormat(NumberFormatOptions.custom(
+          minimumIntegerDigits: 5,
+          useGrouping: Grouping.never,
+        ))
+        .format(number);
+    expect(formatter(540), '00540');
+  });
+
+  testWithFormatting('fractionDigits', () {
+    String formatter(Object number) => intl
+        .numberFormat(NumberFormatOptions.custom(
+          digits: const Digits.withSignificantDigits(maximum: 1),
+          useGrouping: Grouping.never,
+        ))
+        .format(number);
+    expect(formatter(540), '500');
+  });
+
+  testWithFormatting('significantDigits', () {
+    final numberFormatOptions = intl.numberFormat(NumberFormatOptions.custom(
+      digits: const Digits.withSignificantDigits(minimum: 1, maximum: 3),
+    ));
+
+    expect(numberFormatOptions.format(3), '3');
+    expect(numberFormatOptions.format(3.1), '3.1');
+    expect(numberFormatOptions.format(3.12), '3.12');
+    expect(numberFormatOptions.format(3.123), '3.12');
+  });
+
+  testWithFormatting('fractionDigits', () {
+    String formatter(Object number) => intl
+        .numberFormat(NumberFormatOptions.custom(
+          minimumIntegerDigits: 3,
+          digits: const Digits.withFractionDigits(minimum: 4),
+        ))
+        .format(number);
+    expect(formatter(4.33), '004.3300');
+  });
+
+  testWithFormatting('RoundingMode', () {
+    for (final roundingMode in RoundingMode.values) {
+      final expectation = switch (roundingMode) {
+        RoundingMode.ceil => [2.3, 2.3, 2.3, -2.2, -2.2, -2.2],
+        RoundingMode.floor => [2.2, 2.2, 2.2, -2.3, -2.3, -2.3],
+        RoundingMode.expand => [2.3, 2.3, 2.3, -2.3, -2.3, -2.3],
+        RoundingMode.trunc => [2.2, 2.2, 2.2, -2.2, -2.2, -2.2],
+        RoundingMode.halfCeil => [2.2, 2.3, 2.3, -2.2, -2.2, -2.3],
+        RoundingMode.halfFloor => [2.2, 2.2, 2.3, -2.2, -2.3, -2.3],
+        RoundingMode.halfExpand => [2.2, 2.3, 2.3, -2.2, -2.3, -2.3],
+        RoundingMode.halfTrunc => [2.2, 2.2, 2.3, -2.2, -2.2, -2.3],
+        RoundingMode.halfEven => [2.2, 2.2, 2.3, -2.2, -2.2, -2.3],
+      };
+      String formatter(Object number) => intl
+          .numberFormat(NumberFormatOptions.custom(
+              roundingMode: roundingMode,
+              digits: const Digits.withSignificantDigits(maximum: 2)))
+          .format(number);
+      final input = [2.23, 2.25, 2.28, -2.23, -2.25, -2.28];
+      for (var i = 0; i < input.length; i++) {
+        expect(formatter(input[i]), expectation[i].toString(),
+            reason: 'In rounding mode ${roundingMode.name}');
+      }
+    }
+  });
+  group('description', () {
+    testWithFormatting('RoundingPriority', () {
+      String formatter(Object number, Digits digits) => intl
+          .numberFormat(NumberFormatOptions.custom(digits: digits))
+          .format(number);
+      // expect(
+      //     formatter(
+      //         1.23456,
+      //         const Digits.withSignificantAndFractionDigits(
+      //           roundingPriority: RoundingPriority.lessPrecision,
+      //           maximumSignificantDigits: 3,
+      //           maximumFractionDigits: 3,
+      //         )),
+      //     '1.23');
+
+      expect(
+          formatter(
+              1.23456,
+              const Digits.withSignificantAndFractionDigits(
+                roundingPriority: RoundingPriority.morePrecision,
+                maximumSignificantDigits: 3,
+                maximumFractionDigits: 3,
+              )),
+          '1.235');
     });
   });
 }
