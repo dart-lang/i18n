c41d24d38248a114ec8640076657ff8164e9df6b
Moritz <mosum@google.com>
2024-02-09 12:25:08 +0100

    Add support for display names from ICU4X

--- pkgs/intl4x/lib/src/display_names/display_names_4x.dart
+++ pkgs/intl4x/lib/src/display_names/display_names_4x.dart
@@ -2,11 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import '../../display_names.dart';
+import '../bindings/lib.g.dart' as icu;
 import '../data.dart';
+import '../data_4x.dart';
 import '../locale/locale.dart';
-import '../options.dart';
+import '../locale/locale_4x.dart';
 import 'display_names_impl.dart';
-import 'display_names_options.dart';
 
 DisplayNamesImpl getDisplayNames4X(
   Locale locale,
@@ -16,35 +18,69 @@ DisplayNamesImpl getDisplayNames4X(
     DisplayNames4X(locale, data, options);
 
 class DisplayNames4X extends DisplayNamesImpl {
-  DisplayNames4X(super.locale, Data data, super.options);
+  final icu.LocaleDisplayNamesFormatter _formatter;
+  final icu.RegionDisplayNames _regionFormatter;
+
+  DisplayNames4X(super.locale, Data data, super.options)
+      : _formatter = icu.LocaleDisplayNamesFormatter(
+          data.to4X(),
+          locale.to4X(),
+          options.to4X(),
+        ),
+        _regionFormatter = icu.RegionDisplayNames(
+          data.to4X(),
+          locale.to4X(),
+        );
 
   @override
   String ofCalendar(Calendar calendar) {
-    throw UnimplementedError('Insert diplomat bindings here');
+    throw UnsupportedError('Insert diplomat bindings here');
   }
 
   @override
   String ofCurrency(String currencyCode) {
-    throw UnimplementedError('Insert diplomat bindings here');
+    throw UnsupportedError('Insert diplomat bindings here');
   }
 
   @override
   String ofDateTime(DateTimeField field) {
-    throw UnimplementedError('Insert diplomat bindings here');
+    throw UnsupportedError('Insert diplomat bindings here');
   }
 
   @override
-  String ofLanguage(Locale locale) {
-    throw UnimplementedError('Insert diplomat bindings here');
-  }
+  String ofLanguage(Locale locale) => _formatter.of(locale.to4X());
 
   @override
-  String ofRegion(String regionCode) {
-    throw UnimplementedError('Insert diplomat bindings here');
-  }
+  String ofRegion(String regionCode) => _regionFormatter.of(regionCode);
 
   @override
   String ofScript(String scriptCode) {
-    throw UnimplementedError('Insert diplomat bindings here');
+    throw UnsupportedError('Insert diplomat bindings here');
+  }
+}
+
+extension on DisplayNamesOptions {
+  icu.DisplayNamesOptions to4X() {
+    final icuStyle = switch (style) {
+      Style.narrow => icu.DisplayNamesStyle.narrow,
+      Style.short => icu.DisplayNamesStyle.short,
+      Style.long => icu.DisplayNamesStyle.long,
+    };
+
+    final icuFallback = switch (fallback) {
+      Fallback.code => icu.DisplayNamesFallback.code,
+      Fallback.none => icu.DisplayNamesFallback.none,
+    };
+
+    final icuLanguageDisplay = switch (languageDisplay) {
+      LanguageDisplay.dialect => icu.LanguageDisplay.dialect,
+      LanguageDisplay.standard => icu.LanguageDisplay.standard,
+    };
+
+    return icu.DisplayNamesOptions(
+      style: icuStyle,
+      fallback: icuFallback,
+      languageDisplay: icuLanguageDisplay,
+    );
   }
 }
---
+++ pkgs/intl4x/test/display_names_test.dart
@@ -0,0 +1,87 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:intl4x/display_names.dart';
+import 'package:intl4x/intl4x.dart';
+import 'package:test/test.dart';
+
+import 'utils.dart';
+
+void main() {
+  testWithFormatting('basic', () {
+    expect(
+        Intl(locale: const Locale(language: 'en', region: 'US'))
+            .displayNames()
+            .ofLanguage(const Locale(language: 'de', region: 'DE')),
+        'German (Germany)');
+  });
+
+  testWithFormatting('languageDisplay', () {
+    String of(DisplayNamesOptions options) =>
+        Intl(locale: const Locale(language: 'en'))
+            .displayNames(options)
+            .ofLanguage(const Locale(language: 'en', region: 'GB'));
+
+    expect(
+      of(const DisplayNamesOptions(languageDisplay: LanguageDisplay.dialect)),
+      'British English',
+    );
+    expect(
+      of(const DisplayNamesOptions(languageDisplay: LanguageDisplay.standard)),
+      'English (United Kingdom)',
+    );
+  });
+
+  testWithFormatting('calendar', () {
+    final displayNames =
+        Intl(locale: const Locale(language: 'en')).displayNames();
+
+    expect(displayNames.ofCalendar(Calendar.roc), 'Minguo Calendar');
+    expect(displayNames.ofCalendar(Calendar.gregory), 'Gregorian Calendar');
+    expect(displayNames.ofCalendar(Calendar.chinese), 'Chinese Calendar');
+  });
+
+  testWithFormatting('dateTimeField', () {
+    final displayNames =
+        Intl(locale: const Locale(language: 'pt')).displayNames();
+    expect(displayNames.ofDateTime(DateTimeField.era), 'era');
+    expect(displayNames.ofDateTime(DateTimeField.year), 'ano');
+    expect(displayNames.ofDateTime(DateTimeField.month), 'mês');
+    expect(displayNames.ofDateTime(DateTimeField.quarter), 'trimestre');
+    expect(displayNames.ofDateTime(DateTimeField.weekOfYear), 'semana');
+    expect(displayNames.ofDateTime(DateTimeField.weekday), 'dia da semana');
+    expect(displayNames.ofDateTime(DateTimeField.dayPeriod), 'AM/PM');
+    expect(displayNames.ofDateTime(DateTimeField.day), 'dia');
+    expect(displayNames.ofDateTime(DateTimeField.hour), 'hora');
+    expect(displayNames.ofDateTime(DateTimeField.minute), 'minuto');
+    expect(displayNames.ofDateTime(DateTimeField.second), 'segundo');
+  });
+
+  testWithFormatting('currency', () {
+    expect(
+      Intl(locale: const Locale(language: 'pt'))
+          .displayNames()
+          .ofCurrency('USD'),
+      'Dólar americano',
+    );
+  });
+
+  testWithFormatting('script', () {
+    expect(
+      Intl(locale: const Locale(language: 'fr'))
+          .displayNames()
+          .ofScript('Egyp'),
+      'hiéroglyphes égyptiens',
+    );
+  });
+
+  testWithFormatting('region', () {
+    expect(
+      Intl(locale: const Locale(language: 'es', region: '419'))
+          .displayNames()
+          .ofRegion('DE'),
+      'Alemania',
+    );
+  });
+}
