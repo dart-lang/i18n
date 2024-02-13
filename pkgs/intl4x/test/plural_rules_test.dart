709bdcd4ace9a2bb0fdfad06686c4691f2547d53
Moritz <mosum@google.com>
2024-02-13 11:26:53 +0100

    Add support for plural rules from ICU4X (#799)

--- pkgs/intl4x/test/plural_rules_test.dart
+++ pkgs/intl4x/test/plural_rules_test.dart
@@ -1,15 +1,11 @@
 // Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-
-@TestOn('browser')
-library;
-
 import 'package:intl4x/intl4x.dart';
 import 'package:intl4x/plural_rules.dart';
 import 'package:test/test.dart';
 
-import '../utils.dart';
+import 'utils.dart';
 
 void main() {
   testWithFormatting('en-US simple', () {
