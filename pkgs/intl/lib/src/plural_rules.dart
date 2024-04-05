19014a0f32b5217b631e1879b069806c90700367
Googler <copybara-servicebot@google.com>
2024-04-05 14:48:47 -0700

    No public description
    PiperOrigin-RevId: 622287675

--- pkgs/intl/lib/src/plural_rules.dart
+++ pkgs/intl/lib/src/plural_rules.dart
@@ -133,491 +133,307 @@ PluralCase get MANY => PluralCase.MANY;
 PluralCase get OTHER => PluralCase.OTHER;
 
 PluralCase _ast_rule() {
-  if ((_i == 1) && (_v == 0)) {
-    return ONE;
-  }
-  return OTHER;
+if((_i == 1)&&(_v == 0)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _af_rule() {
-  if (_n == 1) {
-    return ONE;
-  }
-  return OTHER;
+if((_n == 1)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _am_rule() {
-  if ((_i == 0) || (_n == 1)) {
-    return ONE;
-  }
-  return OTHER;
+if((_i == 0)||(_n == 1)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _ar_rule() {
-  if (_n == 0) {
-    return ZERO;
-  }
-  if (_n == 1) {
-    return ONE;
-  }
-  if (_n == 2) {
-    return TWO;
-  }
-  if ([3, 4, 5, 6, 7, 8, 9, 10].contains(_n % 100)) {
-    return FEW;
-  }
-  if (List.generate(89, (i) => i + 11).contains(_n % 100)) {
-    return MANY;
-  }
-  return OTHER;
+if((_n == 0)){ return ZERO; }
+if((_n == 1)){ return ONE; }
+if((_n == 2)){ return TWO; }
+if(([3, 4, 5, 6, 7, 8, 9, 10].contains(_n % 100))){ return FEW; }
+if((List.generate(89, (i) => i + 11).contains(_n % 100))){ return MANY; }
+return OTHER;
 }
 
 PluralCase _be_rule() {
-  if ((_n % 10 == 1) && !(_n % 100 == 11)) {
-    return ONE;
-  }
-  if ((_n % 10 == 2 || _n % 10 == 3 || _n % 10 == 4) &&
-      !(_n % 100 == 12 || _n % 100 == 13 || _n % 100 == 14)) {
-    return FEW;
-  }
-  if ((_n % 10 == 0) ||
-      (_n % 10 == 5 ||
-          _n % 10 == 6 ||
-          _n % 10 == 7 ||
-          _n % 10 == 8 ||
-          _n % 10 == 9) ||
-      (_n % 100 == 11 || _n % 100 == 12 || _n % 100 == 13 || _n % 100 == 14)) {
-    return MANY;
-  }
-  return OTHER;
+if((_n % 10 == 1)&&!(_n % 100 == 11)){ return ONE; }
+if((_n % 10 == 2||_n % 10 == 3||_n % 10 == 4)&&!(_n % 100 == 12||_n % 100 == 13||_n % 100 == 14)){ return FEW; }
+if((_n % 10 == 0)||(_n % 10 == 5||_n % 10 == 6||_n % 10 == 7||_n % 10 == 8||_n % 10 == 9)||(_n % 100 == 11||_n % 100 == 12||_n % 100 == 13||_n % 100 == 14)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _br_rule() {
-  if ((_n % 10 == 1) && !(_n % 100 == 11 || _n % 100 == 71 || _n % 100 == 91)) {
-    return ONE;
-  }
-  if ((_n % 10 == 2) && !(_n % 100 == 12 || _n % 100 == 72 || _n % 100 == 92)) {
-    return TWO;
-  }
-  if ((_n % 10 == 3 || _n % 10 == 4 || _n % 10 == 9) &&
-      !([10, 11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_n % 100) ||
-          [70, 71, 72, 73, 74, 75, 76, 77, 78, 79].contains(_n % 100) ||
-          [90, 91, 92, 93, 94, 95, 96, 97, 98, 99].contains(_n % 100))) {
-    return FEW;
-  }
-  if (!(_n == 0) && (_n % 1000000 == 0)) {
-    return MANY;
-  }
-  return OTHER;
+if((_n % 10 == 1)&&!(_n % 100 == 11||_n % 100 == 71||_n % 100 == 91)){ return ONE; }
+if((_n % 10 == 2)&&!(_n % 100 == 12||_n % 100 == 72||_n % 100 == 92)){ return TWO; }
+if((_n % 10 == 3||_n % 10 == 4||_n % 10 == 9)&&!([10, 11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_n % 100)||[70, 71, 72, 73, 74, 75, 76, 77, 78, 79].contains(_n % 100)||[90, 91, 92, 93, 94, 95, 96, 97, 98, 99].contains(_n % 100))){ return FEW; }
+if(!(_n == 0)&&(_n % 1000000 == 0)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _bs_rule() {
-  if ((_v == 0) && (_i % 10 == 1) && !(_i % 100 == 11) ||
-      (_f % 10 == 1) && !(_f % 100 == 11)) {
-    return ONE;
-  }
-  if ((_v == 0) &&
-          (_i % 10 == 2 || _i % 10 == 3 || _i % 10 == 4) &&
-          !(_i % 100 == 12 || _i % 100 == 13 || _i % 100 == 14) ||
-      (_f % 10 == 2 || _f % 10 == 3 || _f % 10 == 4) &&
-          !(_f % 100 == 12 || _f % 100 == 13 || _f % 100 == 14)) {
-    return FEW;
-  }
-  return OTHER;
+if((_v == 0)&&(_i % 10 == 1)&&!(_i % 100 == 11)||(_f % 10 == 1)&&!(_f % 100 == 11)){ return ONE; }
+if((_v == 0)&&(_i % 10 == 2||_i % 10 == 3||_i % 10 == 4)&&!(_i % 100 == 12||_i % 100 == 13||_i % 100 == 14)||(_f % 10 == 2||_f % 10 == 3||_f % 10 == 4)&&!(_f % 100 == 12||_f % 100 == 13||_f % 100 == 14)){ return FEW; }
+return OTHER;
 }
 
 PluralCase _ca_rule() {
-  if ((_i == 1) && (_v == 0)) {
-    return ONE;
-  }
-  if (!(_i == 0) && (_i % 1000000 == 0) && (_v == 0)) {
-    return MANY;
-  }
-  return OTHER;
+if((_i == 1)&&(_v == 0)){ return ONE; }
+if(!(_i == 0)&&(_i % 1000000 == 0)&&(_v == 0)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _cs_rule() {
-  if ((_i == 1) && (_v == 0)) {
-    return ONE;
-  }
-  if ((_i == 2 || _i == 3 || _i == 4) && (_v == 0)) {
-    return FEW;
-  }
-  if (!(_v == 0)) {
-    return MANY;
-  }
-  return OTHER;
+if((_i == 1)&&(_v == 0)){ return ONE; }
+if((_i == 2||_i == 3||_i == 4)&&(_v == 0)){ return FEW; }
+if(!(_v == 0)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _cy_rule() {
-  if (_n == 0) {
-    return ZERO;
-  }
-  if (_n == 1) {
-    return ONE;
-  }
-  if (_n == 2) {
-    return TWO;
-  }
-  if (_n == 3) {
-    return FEW;
-  }
-  if (_n == 6) {
-    return MANY;
-  }
-  return OTHER;
+if((_n == 0)){ return ZERO; }
+if((_n == 1)){ return ONE; }
+if((_n == 2)){ return TWO; }
+if((_n == 3)){ return FEW; }
+if((_n == 6)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _da_rule() {
-  if ((_n == 1) || !(_t == 0) && (_i == 0 || _i == 1)) {
-    return ONE;
-  }
-  return OTHER;
+if((_n == 1)||!(_t == 0)&&(_i == 0||_i == 1)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _es_rule() {
-  if (_n == 1) {
-    return ONE;
-  }
-  if (!(_i == 0) && (_i % 1000000 == 0) && (_v == 0)) {
-    return MANY;
-  }
-  return OTHER;
+if((_n == 1)){ return ONE; }
+if(!(_i == 0)&&(_i % 1000000 == 0)&&(_v == 0)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _ceb_rule() {
-  if ((_v == 0) && (_i == 1 || _i == 2 || _i == 3) ||
-      (_v == 0) && !(_i % 10 == 4 || _i % 10 == 6 || _i % 10 == 9) ||
-      !(_v == 0) && !(_f % 10 == 4 || _f % 10 == 6 || _f % 10 == 9)) {
-    return ONE;
-  }
-  return OTHER;
+if((_v == 0)&&(_i == 1||_i == 2||_i == 3)||(_v == 0)&&!(_i % 10 == 4||_i % 10 == 6||_i % 10 == 9)||!(_v == 0)&&!(_f % 10 == 4||_f % 10 == 6||_f % 10 == 9)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _fr_rule() {
-  if (_i == 0 || _i == 1) {
-    return ONE;
-  }
-  if (!(_i == 0) && (_i % 1000000 == 0) && (_v == 0)) {
-    return MANY;
-  }
-  return OTHER;
+if((_i == 0||_i == 1)){ return ONE; }
+if(!(_i == 0)&&(_i % 1000000 == 0)&&(_v == 0)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _ga_rule() {
-  if (_n == 1) {
-    return ONE;
-  }
-  if (_n == 2) {
-    return TWO;
-  }
-  if (_n == 3 || _n == 4 || _n == 5 || _n == 6) {
-    return FEW;
-  }
-  if (_n == 7 || _n == 8 || _n == 9 || _n == 10) {
-    return MANY;
-  }
-  return OTHER;
+if((_n == 1)){ return ONE; }
+if((_n == 2)){ return TWO; }
+if((_n == 3||_n == 4||_n == 5||_n == 6)){ return FEW; }
+if((_n == 7||_n == 8||_n == 9||_n == 10)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _he_rule() {
-  if ((_i == 1) && (_v == 0) || (_i == 0) && !(_v == 0)) {
-    return ONE;
-  }
-  if ((_i == 2) && (_v == 0)) {
-    return TWO;
-  }
-  return OTHER;
+if((_i == 1)&&(_v == 0)||(_i == 0)&&!(_v == 0)){ return ONE; }
+if((_i == 2)&&(_v == 0)){ return TWO; }
+return OTHER;
 }
 
 PluralCase _ff_rule() {
-  if (_i == 0 || _i == 1) {
-    return ONE;
-  }
-  return OTHER;
+if((_i == 0||_i == 1)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _is_rule() {
-  if ((_t == 0) && (_i % 10 == 1) && !(_i % 100 == 11) ||
-      (_t % 10 == 1) && !(_t % 100 == 11)) {
-    return ONE;
-  }
-  return OTHER;
+if((_t == 0)&&(_i % 10 == 1)&&!(_i % 100 == 11)||(_t % 10 == 1)&&!(_t % 100 == 11)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _ak_rule() {
-  if (_n == 0 || _n == 1) {
-    return ONE;
-  }
-  return OTHER;
+if((_n == 0||_n == 1)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _lt_rule() {
-  if ((_n % 10 == 1) &&
-      ![11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_n % 100)) {
-    return ONE;
-  }
-  if (([2, 3, 4, 5, 6, 7, 8, 9].contains(_n % 10)) &&
-      ![11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_n % 100)) {
-    return FEW;
-  }
-  if (!(_f == 0)) {
-    return MANY;
-  }
-  return OTHER;
+if((_n % 10 == 1)&&!([11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_n % 100))){ return ONE; }
+if(([2, 3, 4, 5, 6, 7, 8, 9].contains(_n % 10))&&!([11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_n % 100))){ return FEW; }
+if(!(_f == 0)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _lv_rule() {
-  if ((_n % 10 == 0) ||
-      ([11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_n % 100)) ||
-      (_v == 2) && ([11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_f % 100))) {
-    return ZERO;
-  }
-  if ((_n % 10 == 1) && !(_n % 100 == 11) ||
-      (_v == 2) && (_f % 10 == 1) && !(_f % 100 == 11) ||
-      !(_v == 2) && (_f % 10 == 1)) {
-    return ONE;
-  }
-  return OTHER;
+if((_n % 10 == 0)||([11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_n % 100))||(_v == 2)&&([11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_f % 100))){ return ZERO; }
+if((_n % 10 == 1)&&!(_n % 100 == 11)||(_v == 2)&&(_f % 10 == 1)&&!(_f % 100 == 11)||!(_v == 2)&&(_f % 10 == 1)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _mk_rule() {
-  if ((_v == 0) && (_i % 10 == 1) && !(_i % 100 == 11) ||
-      (_f % 10 == 1) && !(_f % 100 == 11)) {
-    return ONE;
-  }
-  return OTHER;
+if((_v == 0)&&(_i % 10 == 1)&&!(_i % 100 == 11)||(_f % 10 == 1)&&!(_f % 100 == 11)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _mt_rule() {
-  if (_n == 1) {
-    return ONE;
-  }
-  if (_n == 2) {
-    return TWO;
-  }
-  if ((_n == 0) || ([3, 4, 5, 6, 7, 8, 9, 10].contains(_n % 100))) {
-    return FEW;
-  }
-  if ([11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_n % 100)) {
-    return MANY;
-  }
-  return OTHER;
+if((_n == 1)){ return ONE; }
+if((_n == 2)){ return TWO; }
+if((_n == 0)||([3, 4, 5, 6, 7, 8, 9, 10].contains(_n % 100))){ return FEW; }
+if(([11, 12, 13, 14, 15, 16, 17, 18, 19].contains(_n % 100))){ return MANY; }
+return OTHER;
 }
 
 PluralCase _pl_rule() {
-  if ((_i == 1) && (_v == 0)) {
-    return ONE;
-  }
-  if ((_v == 0) &&
-      (_i % 10 == 2 || _i % 10 == 3 || _i % 10 == 4) &&
-      !(_i % 100 == 12 || _i % 100 == 13 || _i % 100 == 14)) {
-    return FEW;
-  }
-  if ((_v == 0) && !(_i == 1) && (_i % 10 == 0 || _i % 10 == 1) ||
-      (_v == 0) &&
-          (_i % 10 == 5 ||
-              _i % 10 == 6 ||
-              _i % 10 == 7 ||
-              _i % 10 == 8 ||
-              _i % 10 == 9) ||
-      (_v == 0) && (_i % 100 == 12 || _i % 100 == 13 || _i % 100 == 14)) {
-    return MANY;
-  }
-  return OTHER;
+if((_i == 1)&&(_v == 0)){ return ONE; }
+if((_v == 0)&&(_i % 10 == 2||_i % 10 == 3||_i % 10 == 4)&&!(_i % 100 == 12||_i % 100 == 13||_i % 100 == 14)){ return FEW; }
+if((_v == 0)&&!(_i == 1)&&(_i % 10 == 0||_i % 10 == 1)||(_v == 0)&&(_i % 10 == 5||_i % 10 == 6||_i % 10 == 7||_i % 10 == 8||_i % 10 == 9)||(_v == 0)&&(_i % 100 == 12||_i % 100 == 13||_i % 100 == 14)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _pt_rule() {
-  if (_i == 0 || _i == 1) {
-    return ONE;
-  }
-  if (!(_i == 0) && (_i % 1000000 == 0) && (_v == 0)) {
-    return MANY;
-  }
-  return OTHER;
+if((_i == 0||_i == 1)){ return ONE; }
+if(!(_i == 0)&&(_i % 1000000 == 0)&&(_v == 0)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _mo_rule() {
-  if ((_i == 1) && (_v == 0)) {
-    return ONE;
-  }
-  if (!(_v == 0) ||
-      (_n == 0) ||
-      !(_n == 1) && (List.generate(19, (i) => i + 1).contains(_n % 100))) {
-    return FEW;
-  }
-  return OTHER;
+if((_i == 1)&&(_v == 0)){ return ONE; }
+if(!(_v == 0)||(_n == 0)||!(_n == 1)&&(List.generate(19, (i) => i + 1).contains(_n % 100))){ return FEW; }
+return OTHER;
 }
 
 PluralCase _ru_rule() {
-  if ((_v == 0) && (_i % 10 == 1) && !(_i % 100 == 11)) {
-    return ONE;
-  }
-  if ((_v == 0) &&
-      (_i % 10 == 2 || _i % 10 == 3 || _i % 10 == 4) &&
-      !(_i % 100 == 12 || _i % 100 == 13 || _i % 100 == 14)) {
-    return FEW;
-  }
-  if ((_v == 0) && (_i % 10 == 0) ||
-      (_v == 0) &&
-          (_i % 10 == 5 ||
-              _i % 10 == 6 ||
-              _i % 10 == 7 ||
-              _i % 10 == 8 ||
-              _i % 10 == 9) ||
-      (_v == 0) &&
-          (_i % 100 == 11 ||
-              _i % 100 == 12 ||
-              _i % 100 == 13 ||
-              _i % 100 == 14)) {
-    return MANY;
-  }
-  return OTHER;
+if((_v == 0)&&(_i % 10 == 1)&&!(_i % 100 == 11)){ return ONE; }
+if((_v == 0)&&(_i % 10 == 2||_i % 10 == 3||_i % 10 == 4)&&!(_i % 100 == 12||_i % 100 == 13||_i % 100 == 14)){ return FEW; }
+if((_v == 0)&&(_i % 10 == 0)||(_v == 0)&&(_i % 10 == 5||_i % 10 == 6||_i % 10 == 7||_i % 10 == 8||_i % 10 == 9)||(_v == 0)&&(_i % 100 == 11||_i % 100 == 12||_i % 100 == 13||_i % 100 == 14)){ return MANY; }
+return OTHER;
 }
 
 PluralCase _si_rule() {
-  if ((_n == 0 || _n == 1) || (_i == 0) && (_f == 1)) {
-    return ONE;
-  }
-  return OTHER;
+if((_n == 0||_n == 1)||(_i == 0)&&(_f == 1)){ return ONE; }
+return OTHER;
 }
 
 PluralCase _sl_rule() {
-  if ((_v == 0) && (_i % 100 == 1)) {
-    return ONE;
-  }
-  if ((_v == 0) && (_i % 100 == 2)) {
-    return TWO;
-  }
-  if ((_v == 0) && (_i % 100 == 3 || _i % 100 == 4) || !(_v == 0)) {
-    return FEW;
-  }
-  return OTHER;
+if((_v == 0)&&(_i % 100 == 1)){ return ONE; }
+if((_v == 0)&&(_i % 100 == 2)){ return TWO; }
+if((_v == 0)&&(_i % 100 == 3||_i % 100 == 4)||!(_v == 0)){ return FEW; }
+return OTHER;
 }
-
-final pluralRules = {
-  'en_ISO': _ast_rule,
-  'af': _af_rule,
-  'am': _am_rule,
-  'ar': _ar_rule,
-  'ar_DZ': _ar_rule,
-  'ar_EG': _ar_rule,
-  'as': _am_rule,
-  'az': _af_rule,
-  'be': _be_rule,
-  'bg': _af_rule,
-  'bm': _default_rule,
-  'bn': _am_rule,
-  'br': _br_rule,
-  'bs': _bs_rule,
-  'ca': _ca_rule,
-  'chr': _af_rule,
-  'cs': _cs_rule,
-  'cy': _cy_rule,
-  'da': _da_rule,
-  'de': _ast_rule,
-  'de_AT': _ast_rule,
-  'de_CH': _ast_rule,
-  'el': _af_rule,
-  'en': _ast_rule,
-  'en_AU': _ast_rule,
-  'en_CA': _ast_rule,
-  'en_GB': _ast_rule,
-  'en_IE': _ast_rule,
-  'en_IN': _ast_rule,
-  'en_MY': _ast_rule,
-  'en_NZ': _ast_rule,
-  'en_SG': _ast_rule,
-  'en_US': _ast_rule,
-  'en_ZA': _ast_rule,
-  'es': _es_rule,
-  'es_419': _es_rule,
-  'es_ES': _es_rule,
-  'es_MX': _es_rule,
-  'es_US': _es_rule,
-  'et': _ast_rule,
-  'eu': _af_rule,
-  'fa': _am_rule,
-  'fi': _ast_rule,
-  'fil': _ceb_rule,
-  'fr': _fr_rule,
-  'fr_CA': _fr_rule,
-  'fr_CH': _fr_rule,
-  'fur': _af_rule,
-  'ga': _ga_rule,
-  'gl': _ast_rule,
-  'gsw': _af_rule,
-  'gu': _am_rule,
-  'haw': _af_rule,
-  'he': _he_rule,
-  'hi': _am_rule,
-  'hr': _bs_rule,
-  'hu': _af_rule,
-  'hy': _ff_rule,
-  'id': _default_rule,
-  'in': _default_rule,
-  'is': _is_rule,
-  'it': _ca_rule,
-  'it_CH': _ca_rule,
-  'iw': _he_rule,
-  'ja': _default_rule,
-  'ka': _af_rule,
-  'kk': _af_rule,
-  'km': _default_rule,
-  'kn': _am_rule,
-  'ko': _default_rule,
-  'ky': _af_rule,
-  'ln': _ak_rule,
-  'lo': _default_rule,
-  'lt': _lt_rule,
-  'lv': _lv_rule,
-  'mg': _ak_rule,
-  'mk': _mk_rule,
-  'ml': _af_rule,
-  'mn': _af_rule,
-  'mr': _af_rule,
-  'ms': _default_rule,
-  'mt': _mt_rule,
-  'my': _default_rule,
-  'nb': _af_rule,
-  'ne': _af_rule,
-  'nl': _ast_rule,
-  'no': _af_rule,
-  'no_NO': _af_rule,
-  'nyn': _af_rule,
-  'or': _af_rule,
-  'pa': _ak_rule,
-  'pl': _pl_rule,
-  'ps': _af_rule,
-  'pt': _pt_rule,
-  'pt_BR': _pt_rule,
-  'pt_PT': _ca_rule,
-  'ro': _mo_rule,
-  'ru': _ru_rule,
-  'si': _si_rule,
-  'sk': _cs_rule,
-  'sl': _sl_rule,
-  'sq': _af_rule,
-  'sr': _bs_rule,
-  'sr_Latn': _bs_rule,
-  'sv': _ast_rule,
-  'sw': _ast_rule,
-  'ta': _af_rule,
-  'te': _af_rule,
-  'th': _default_rule,
-  'tl': _ceb_rule,
-  'tr': _af_rule,
-  'uk': _ru_rule,
-  'ur': _ast_rule,
-  'uz': _af_rule,
-  'vi': _default_rule,
-  'zh': _default_rule,
-  'zh_CN': _default_rule,
-  'zh_HK': _default_rule,
-  'zh_TW': _default_rule,
-  'zu': _am_rule,
-  'default': _default_rule,
-};
-
+final pluralRules = {'en_ISO': _ast_rule,
+'af': _af_rule,
+'am': _am_rule,
+'ar': _ar_rule,
+'ar_DZ': _ar_rule,
+'ar_EG': _ar_rule,
+'as': _am_rule,
+'az': _af_rule,
+'be': _be_rule,
+'bg': _af_rule,
+'bm': _default_rule,
+'bn': _am_rule,
+'br': _br_rule,
+'bs': _bs_rule,
+'ca': _ca_rule,
+'chr': _af_rule,
+'cs': _cs_rule,
+'cy': _cy_rule,
+'da': _da_rule,
+'de': _ast_rule,
+'de_AT': _ast_rule,
+'de_CH': _ast_rule,
+'el': _af_rule,
+'en': _ast_rule,
+'en_AU': _ast_rule,
+'en_CA': _ast_rule,
+'en_GB': _ast_rule,
+'en_IE': _ast_rule,
+'en_IN': _ast_rule,
+'en_MY': _ast_rule,
+'en_NZ': _ast_rule,
+'en_SG': _ast_rule,
+'en_US': _ast_rule,
+'en_ZA': _ast_rule,
+'es': _es_rule,
+'es_419': _es_rule,
+'es_ES': _es_rule,
+'es_MX': _es_rule,
+'es_US': _es_rule,
+'et': _ast_rule,
+'eu': _af_rule,
+'fa': _am_rule,
+'fi': _ast_rule,
+'fil': _ceb_rule,
+'fr': _fr_rule,
+'fr_CA': _fr_rule,
+'fr_CH': _fr_rule,
+'fur': _af_rule,
+'ga': _ga_rule,
+'gl': _ast_rule,
+'gsw': _af_rule,
+'gu': _am_rule,
+'haw': _af_rule,
+'he': _he_rule,
+'hi': _am_rule,
+'hr': _bs_rule,
+'hu': _af_rule,
+'hy': _ff_rule,
+'id': _default_rule,
+'in': _default_rule,
+'is': _is_rule,
+'it': _ca_rule,
+'it_CH': _ca_rule,
+'iw': _he_rule,
+'ja': _default_rule,
+'ka': _af_rule,
+'kk': _af_rule,
+'km': _default_rule,
+'kn': _am_rule,
+'ko': _default_rule,
+'ky': _af_rule,
+'ln': _ak_rule,
+'lo': _default_rule,
+'lt': _lt_rule,
+'lv': _lv_rule,
+'mg': _ak_rule,
+'mk': _mk_rule,
+'ml': _af_rule,
+'mn': _af_rule,
+'mr': _af_rule,
+'ms': _default_rule,
+'mt': _mt_rule,
+'my': _default_rule,
+'nb': _af_rule,
+'ne': _af_rule,
+'nl': _ast_rule,
+'no': _af_rule,
+'no_NO': _af_rule,
+'nyn': _af_rule,
+'or': _af_rule,
+'pa': _ak_rule,
+'pl': _pl_rule,
+'ps': _af_rule,
+'pt': _pt_rule,
+'pt_BR': _pt_rule,
+'pt_PT': _ca_rule,
+'ro': _mo_rule,
+'ru': _ru_rule,
+'si': _si_rule,
+'sk': _cs_rule,
+'sl': _sl_rule,
+'sq': _af_rule,
+'sr': _bs_rule,
+'sr_Latn': _bs_rule,
+'sv': _ast_rule,
+'sw': _ast_rule,
+'ta': _af_rule,
+'te': _af_rule,
+'th': _default_rule,
+'tl': _ceb_rule,
+'tr': _af_rule,
+'uk': _ru_rule,
+'ur': _ast_rule,
+'uz': _af_rule,
+'vi': _default_rule,
+'zh': _default_rule,
+'zh_CN': _default_rule,
+'zh_HK': _default_rule,
+'zh_TW': _default_rule,
+'zu': _am_rule,
+'default': _default_rule,};
 /// Do we have plural rules specific to [locale]
 bool localeHasPluralRules(String locale) => pluralRules.containsKey(locale);
+
