43d625765c5e70f22caf512d07ce9000144cc88f
Moritz <mosum@google.com>
2024-05-27 13:07:48 +0200

    Adapt the example in `package:messages` to use the `AssetBundle`

---
+++ pkgs/messages/example_json/hook/build.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:native_assets_cli/native_assets_cli.dart';
+
+const package = 'example_json';
+void main(List<String> args) {
+  final dir = 'lib/';
+  build(args, (config, output) async {
+    getArbs(config, output, dir, [
+      'testarb',
+      'testarbctx2',
+      'testarbctx2_fr',
+    ]);
+  });
+}
+
+void getArbs(
+  BuildConfig config,
+  BuildOutput output,
+  String dir,
+  List<String> assets,
+) {
+  output.addAssets(assets.map((asset) => getArb(config, dir, asset)));
+  output.addDependencies([
+    config.packageRoot.resolve('hook/build.dart'),
+  ]);
+}
+
+DataAsset getArb(BuildConfig config, String dir, String name) {
+  final directory = Directory(dir);
+  final resolved = directory.uri.resolve('$name.json');
+  return DataAsset(
+    package: package,
+    name: resolved.path,
+    file: config.packageRoot.resolveUri(resolved),
+  );
+}
