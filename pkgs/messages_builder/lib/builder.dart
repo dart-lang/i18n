028583d0cb14ac4a685e8cc6041c55c9d16f88be
Moritz <mosum@google.com>
2024-05-27 20:42:02 +0200

    Refactor a bit

--- pkgs/messages_builder/lib/builder.dart
+++ pkgs/messages_builder/lib/builder.dart
@@ -7,15 +7,13 @@ import 'dart:convert';
 
 import 'package:build/build.dart';
 import 'package:code_builder/code_builder.dart';
-import 'package:code_builder/src/specs/library.dart';
 import 'package:collection/collection.dart';
-import 'package:dart_style/dart_style.dart';
 import 'package:glob/glob.dart';
 import 'package:path/path.dart' as path;
 
 import 'arb_parser.dart';
 import 'code_generation/code.dart';
-import 'code_generation/import_generation.dart';
+import 'code_generation/library_generation.dart';
 import 'generation_options.dart';
 import 'message_with_metadata.dart';
 
@@ -64,114 +62,10 @@ class BuildStepGenerator {
       }
     }
     if (libraries.isNotEmpty) {
-      final imports = ImportGeneration(options).generate();
-
-      Code pluralSelectorBody() {
-        return switch (options.pluralSelector) {
-          PluralSelectorType.intl => const Code('''
-return Intl.pluralLogic(
-    howMany,
-    few: few,
-    many: many,
-    zero: numberCases?[0] ?? wordCases?[0],
-    one: numberCases?[1] ?? wordCases?[1],
-    two: numberCases?[2] ?? wordCases?[2],
-    other: other,
-    locale: currentLocale,
-  );
-  '''),
-          PluralSelectorType.intl4x => const Code('''
-Message getCase(int i) => numberCases?[i] ?? wordCases?[i] ?? other;
-    return switch (Intl(locale: Locale.parse(locale)).plural().select(howMany)) {
-      PluralCategory.zero => getCase(0),
-      PluralCategory.one => getCase(1),
-      PluralCategory.two => getCase(2),
-      PluralCategory.few => few ?? other,
-      PluralCategory.many => many ?? other,
-      PluralCategory.other => other,
-    };
-    '''),
-          PluralSelectorType.custom => throw ArgumentError(),
-        };
-      }
-
-      // Message Function(num,
-      //     String locale,
-      //     {Message? few,
-      //     String? locale,
-      //     Message? many,
-      //     Map<int, Message>? numberCases,
-      //     required Message other,
-      //     Map<int, Message>? wordCases}) intl;
-      Method pluralSelector() => Method(
-            (mb) => mb
-              ..name = '_pluralSelector'
-              ..returns = const Reference('Message')
-              ..requiredParameters.addAll([
-                Parameter(
-                  (pb) => pb
-                    ..name = 'howMany'
-                    ..type = const Reference('num')
-                    ..named = false,
-                ),
-                Parameter(
-                  (pb) => pb
-                    ..name = 'locale'
-                    ..type = const Reference('String')
-                    ..named = false,
-                ),
-              ])
-              ..optionalParameters.addAll([
-                Parameter(
-                  (pb) => pb
-                    ..name = 'other'
-                    ..type = const Reference('Message')
-                    ..required = true
-                    ..named = true,
-                ),
-                Parameter(
-                  (pb) => pb
-                    ..name = 'few'
-                    ..type = const Reference('Message?')
-                    ..named = true,
-                ),
-                Parameter(
-                  (pb) => pb
-                    ..name = 'many'
-                    ..type = const Reference('Message?')
-                    ..named = true,
-                ),
-                Parameter(
-                  (pb) => pb
-                    ..name = 'numberCases'
-                    ..type = const Reference('Map<int, Message>?')
-                    ..named = true,
-                ),
-                Parameter(
-                  (pb) => pb
-                    ..name = 'wordCases'
-                    ..type = const Reference('Map<int, Message>?')
-                    ..named = true,
-                ),
-              ])
-              ..body = pluralSelectorBody(),
-          );
-
-      final lib = libraries.reduce((value, element) => Library(
-            (p0) => p0
-              ..comments.add(options.header)
-              ..directives.addAll(imports)
-              ..body.addAll([
-                ...value.body,
-                ...element.body,
-                if (options.pluralSelector != PluralSelectorType.custom)
-                  pluralSelector(),
-              ]),
-          ));
-
-      final emitter = DartEmitter(orderDirectives: true);
-      final source = '${lib.accept(emitter)}';
-      final code = DartFormatter().format(source);
+      final code = CodeGenerator(
+        options: options,
+        libraries: libraries,
+      ).generate();
 
       await buildStep.writeAsString(
         AssetId(
@@ -217,9 +111,11 @@ Message getCase(int i) => numberCases?[i] ?? wordCases?[i] ?? other;
             )));
 
     printIncludeFilesNotification(messageList.context, localeToResourceInfo);
-    return CodeGenerator(
+    return LibraryGeneration(
       options,
-      messageList,
+      messageList.context,
+      messageList.locale!,
+      messageList.messages,
       localeToResourceInfo,
     ).generate();
   }
