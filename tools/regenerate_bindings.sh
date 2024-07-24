8d63e877646851df9b68ad4609ac10adfdfa72ac
Moritz <mosum@google.com>
2024-07-24 16:52:04 +0200

    Switch to copying ICU4X Dart files (#860)
    * Switch to copying ICU4X files instead of using symlinks
    
    * Switch files to copies
    
    * Switch to Dart fix branch
    
    * Use dart format
    
    * Format files
    
    * Add changelog entry
    
    * Add flags to building libraries
    
    * Add gitattributes
    
    * Add newline

--- tools/regenerate_bindings.sh
+++ tools/regenerate_bindings.sh
@@ -1,3 +1,4 @@
 rm pkgs/intl4x/lib/src/bindings/*
 cd pkgs/intl4x/lib/src/bindings/
-ln -s ../../../../../submodules/icu4x/ffi/capi/bindings/dart/*.dart .
+cp -a ../../../../../submodules/icu4x/ffi/capi/bindings/dart/*.dart .
+dart format .
