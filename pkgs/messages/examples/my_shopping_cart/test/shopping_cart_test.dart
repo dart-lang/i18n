43c4d7fcb600a546db9f03fab24f3bfb9cf89e86
Moritz <mosum@google.com>
2024-10-22 14:47:54 +0200

    Add licenses

--- pkgs/messages/examples/my_shopping_cart/test/shopping_cart_test.dart
+++ pkgs/messages/examples/my_shopping_cart/test/shopping_cart_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:my_shopping_cart/my_shopping_cart.dart';
 import 'package:test/test.dart';
 
