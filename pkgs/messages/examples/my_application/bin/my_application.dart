280649db5bafe05a9b0730981d1d11a56c1686cc
Moritz <mosum@google.com>
2024-10-22 16:09:25 +0200

    Delete old examples

--- pkgs/messages/examples/my_application/bin/my_application.dart
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-import 'package:my_application/my_application.dart';
-
-Future<void> main(List<String> arguments) async {
-  print('Currently: ${await sale()}. ${await items()}!');
-}
