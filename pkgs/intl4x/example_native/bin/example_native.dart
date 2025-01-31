5177bd6958efbf6881da001a6923cc2559788114
Moritz <mosum@google.com>
2025-01-31 17:50:51 +0100

    Add license file

--- pkgs/intl4x/example_native/bin/example_native.dart
+++ pkgs/intl4x/example_native/bin/example_native.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:intl4x/intl4x.dart';
 
 void main(List<String> arguments) {
