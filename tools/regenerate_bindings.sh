a4c6e9d828c1f46566cde8d779f6a182ccc08901
Moritz <mosum@google.com>
2025-03-12 14:41:13 +0100

    fix build libs

--- tools/regenerate_bindings.sh
+++ tools/regenerate_bindings.sh
@@ -1,4 +1,6 @@
 rm pkgs/intl4x/lib/src/bindings/*
 cd pkgs/intl4x/lib/src/bindings/
 cp -a ../../../../../submodules/icu4x/ffi/capi/bindings/dart/*.dart .
+
+cp submodules/icu4x/ffi/dart/tool/build_libs.dart pkgs/intl4x/tool/build_libs.g.dart
 dart format .
