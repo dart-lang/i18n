bb0045870f8ffb140e6cf8e7d9df116e813c033a
Moritz <mosum@google.com>
2025-06-16 09:24:19 +0200

    remove duplicate build_libs

--- tools/regenerate_bindings.sh
+++ tools/regenerate_bindings.sh
@@ -3,8 +3,6 @@ cd pkgs/intl4x/lib/src/bindings/
 cp -a ../../../../../submodules/icu4x/ffi/capi/bindings/dart/*.dart .
 cd ../../../../../
 
-cp submodules/icu4x/ffi/dart/tool/build_libs.dart pkgs/intl4x/tool/build_libs.g.dart
 cp submodules/icu4x/ffi/dart/tool/build_libs.dart pkgs/intl4x/lib/src/hook_helpers/build_libs.g.dart
 dart format pkgs/intl4x/lib/src/bindings/
-dart format pkgs/intl4x/tool/build_libs.g.dart
 dart format pkgs/intl4x/lib/src/hook_helpers/build_libs.g.dart
