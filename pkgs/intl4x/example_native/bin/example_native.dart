cc4db3e12b053fe79f124d8f0ee120a2907058eb
Moritz <mosum@google.com>
2025-06-24 18:23:48 +0200

    more debug

--- pkgs/intl4x/example_native/bin/example_native.dart
+++ pkgs/intl4x/example_native/bin/example_native.dart
@@ -2,11 +2,14 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:intl4x/datetime_format.dart';
 import 'package:intl4x/intl4x.dart';
 
 void main(List<String> arguments) {
   const timeZone = TimeZone.long(name: 'Europe/Paris', offset: '+02:00');
+  print(Platform.localeName);
   print(Intl().locale);
   print(Intl().dateTimeFormat().time(DateTime.now(), timeZone: timeZone));
 }
