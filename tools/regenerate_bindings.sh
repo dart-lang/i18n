3a94152a65308707b723a809613ffbb16a6d2c42
Moritz <mosum@google.com>
2025-06-26 18:24:11 +0200

    Update to main

--- tools/regenerate_bindings.sh
+++ tools/regenerate_bindings.sh
@@ -3,6 +3,6 @@ cd pkgs/intl4x/lib/src/bindings/
 cp -a ../../../../../submodules/icu4x/ffi/capi/bindings/dart/*.dart .
 cd ../../../../../
 
-cp submodules/icu4x/ffi/dart/lib/src/build_libs.dart pkgs/intl4x/lib/src/hook_helpers/build_libs.g.dart
+cp submodules/icu4x/ffi/dart/tool/build_libs.dart pkgs/intl4x/lib/src/hook_helpers/build_libs.g.dart
 dart format pkgs/intl4x/lib/src/bindings/
 dart format pkgs/intl4x/lib/src/hook_helpers/build_libs.g.dart
