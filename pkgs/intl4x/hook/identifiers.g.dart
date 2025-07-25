1e8942cac2db9ba7ff9e0883e0b1cbfec9e22e51
Moritz <mosum@google.com>
2025-07-25 14:11:21 +0200

    Add license

--- pkgs/intl4x/hook/identifiers.g.dart
+++ pkgs/intl4x/hook/identifiers.g.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:record_use/record_use.dart' as record_use;
 
 const ymdtIdentifier = record_use.Identifier(
