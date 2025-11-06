7bf5e2a87a31dd3d7830323f6176973e152ea330
Moritz <mosum@google.com>
2025-11-06 15:49:23 +0100

    export all

--- pkgs/intl4x/lib/intl4x.dart
+++ pkgs/intl4x/lib/intl4x.dart
@@ -2,17 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/locale/locale.dart';
+export 'case_mapping.dart';
+export 'collation.dart';
+export 'datetime_format.dart';
+export 'display_names.dart';
+export 'list_format.dart';
+export 'number_format.dart';
+export 'src/locale/locale.dart' show Locale;
 export 'src/plural_rules/plural_rules.dart' show PluralCategory, PluralRules;
-
-/// The main class for all i18n calls, containing references to other
-/// functions such as
-/// * [numberFormat]
-///
-/// The functionalities are called through getters on an `Intl` instance, i.e.
-/// ```dart
-/// final numberFormat = Intl(
-///   locale: Locale(language: 'en', country: 'US'),
-/// ).numberFormat;
-/// print(numberFormat.percent().format(0.5)); //prints 50%
-/// ```
