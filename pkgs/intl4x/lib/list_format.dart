d76db901da341872cb85a0995de72d6fef8540a4
Moritz <mosum@google.com>
2025-11-18 10:14:03 +0100

    imports

--- pkgs/intl4x/lib/list_format.dart
+++ pkgs/intl4x/lib/list_format.dart
@@ -2,7 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'list_format.dart';
+import 'src/list_format/list_format.dart';
+import 'src/list_format/list_format_options.dart';
+import 'src/locale/locale.dart' show Locale;
 
 export 'src/list_format/list_format.dart' show ListFormat;
 export 'src/list_format/list_format_options.dart' show ListStyle, ListType;
